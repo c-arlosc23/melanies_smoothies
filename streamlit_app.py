3fbd8e6200fc56e81cc91ee8288165d3adf89c37
c-arlosc23 <168128839+c-arlosc23@users.noreply.github.com>
2024-04-25 13:18:14 -0600

    Update streamlit_app.py
    me equivoqué de lugar al importar requests XD

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
 
 # Write directly to the app
 st.title("Example Streamlit App :cup_with_straw:")
@@ -46,4 +43,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {order_name}!',icon="✅")
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
     
